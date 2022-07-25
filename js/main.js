a2e3b5459d1c79e2493b108d487a0819cb249aea
Juan Cruz Cagnoni <juancagnoni@gmail.com>
2022-07-25 15:45:30 -0300

    segunda preentrega .2

--- js/main.js
+++ js/main.js
@@ -21,18 +21,18 @@ function addItemToShoppingCart(itemName, itemPrice, itemImage) {
     const shoppingCartRow = document.createElement('div');
     const shoppingCartContent = `
     <div class="row shoppingCartItem">
-        <div class="col-5">
+        <div class="col-8">
             <div class="shopping-cart-item d-flex align-items-center h-100 border-bottom pb-2 pt-3">
                 <img src=${itemImage} class="shoppingCartImage">
                 <p class="shopping-cart-item-title shoppingCartItemTitle text-truncate ml-3 mb-0">${itemName}</p>
             </div>
         </div>
-        <div class="col-4">
+        <div class="col-2">
             <div class="shopping-cart-price d-flex align-items-center h-100 border-bottom pb-2 pt-3">
                 <p class="item-price mb-0 shoppingCartItemPrice"> ${itemPrice} </p>
             </div>
         </div>
-        <div class="col-3">
+        <div class="col-2">
             <div
                 class="shopping-cart-quantity d-flex justify-content-between align-items-center h-100 border-bottom pb-2 pt-3">
                 <input class="shoppingCartQuantityInput shoppingCartItemQuantity" type="number" value="1">
